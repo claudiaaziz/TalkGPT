64376715d87be00da2457c94c3067663b06f9d50
claudiaaziz <claudiahannahh@gmail.com>
2024-08-17 09:15:11 -0400

    Implement default voice recognition hook

---
+++ hooks/useVoiceRecognition.ts
@@ -0,0 +1,87 @@
+import Voice, {
+	SpeechErrorEvent,
+	SpeechResultsEvent,
+} from '@react-native-voice/voice';
+import { useCallback, useEffect, useState } from 'react';
+
+interface VoiceRecognitionState {
+	recognized: string;
+	pitchLevel: string;
+	errorMessage: string;
+	recordingEnded: string;
+	recordingStarted: string;
+	transcriptionResults: string[];
+	partialTranscriptions: string[];
+	isRecording: boolean;
+}
+
+const useVoiceRecognition = () => {
+	const [voiceState, setVoiceState] = useState<VoiceRecognitionState>({
+		recognized: '',
+		pitchLevel: '',
+		errorMessage: '',
+		recordingEnded: '',
+		recordingStarted: '',
+		transcriptionResults: [],
+		partialTranscriptions: [],
+		isRecording: false,
+	});
+
+	const resetVoiceState = useCallback(() => {
+		setVoiceState({
+			recognized: '',
+			pitchLevel: '',
+			errorMessage: '',
+			recordingEnded: '',
+			recordingStarted: '',
+			transcriptionResults: [],
+			partialTranscriptions: [],
+			isRecording: false,
+		});
+	}, [setVoiceState]);
+
+	const startRecognition = useCallback(async () => {
+		resetVoiceState();
+
+		try {
+			await Voice.start('en-US');
+		} catch (e) {
+			console.error(e);
+		}
+	}, [resetVoiceState]);
+
+	const stopRecognition = useCallback(async () => {
+		try {
+			await Voice.stop();
+		} catch (e) {
+			console.error(e);
+		}
+	}, []);
+
+	const cancelRecognition = useCallback(async () => {
+		try {
+			await Voice.cancel();
+		} catch (e) {
+			console.error(e);
+		}
+	}, []);
+
+	const destroyRecognition = useCallback(async () => {
+		try {
+			await Voice.destroy();
+		} catch (e) {
+			console.error(e);
+		}
+		resetVoiceState();
+	}, [resetVoiceState]);
+
+	return {
+		voiceState,
+		setVoiceState,
+		resetVoiceState,
+		startRecognition,
+		stopRecognition,
+		cancelRecognition,
+		destroyRecognition,
+	};
+};
