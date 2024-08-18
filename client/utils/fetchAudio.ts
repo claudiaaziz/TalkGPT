167fac07ddcb06d319fb2d45c43194e48b434e3d
claudiaaziz <claudiahannahh@gmail.com>
2024-08-17 21:12:34 -0400

    Restore accidentally deleted file

--- client/utils/fetchAudio.ts
+++ client/utils/fetchAudio.ts
@@ -1,8 +1,11 @@
 export const fetchAudio = async (text: string) => {
-	const res = await fetch(process.env.EXPO_PUBLIC_SERVER_PORT!, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ text }),
-	});
+	const res = await fetch(
+		process.env.EXPO_PUBLIC_SERVER_PORT! + 'text-to-speech/synthesize',
+		{
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ text }),
+		}
+	);
 	return await res.blob();
 };
